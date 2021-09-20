aa634e5ecdf9f9bb334f33f57f648f2d3cf3d923
Boubacar Pelage <boubacar.pelage@ynov.com>
2021-09-20 15:09:04 +0200

    Ajout de l'argent

--- index.php
+++ index.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
+<meta charset="utf8" http-equiv='refresh' content='1'>
 <title>Mon jeu IDLE.</title>
+<style type="text/css">
+.formulaire{
+
+}
+</style>
 </head>
 <body>
 <?php
-if(!isset($_POST["magasin_a_acheter"])){
+if(!isset($_POST["magasin_a_acheter"])){ // Si le paramètre magasin_a_acheter n'existe pas.
 	$nombre_total_de_magasins=5;
 	$magasins=array("Stand de limonade","Médias", "Nettoyage de voiture", "Pizza", "Magasin de donut");	
-	echo "<form action=\"index.php\" method=\"post\"> "; //On commence à mettre notre formulaire.
+	$argent_de_depart=5;
+	echo "<strong> Argent :  </strong>";
+	echo($argent_de_depart. " \$");
+	echo "<form action=\"index.php\" method=\"post "; //On commence à mettre notre formulaire.
+	echo "\" class=\" formulaire\"";
 	echo "<br>";
 	for($index=0;$index<$nombre_total_de_magasins;$index++){
 		$magasins_possedes[$index]=0; //Initialisation de la valeur des magasins.
 		$magasin_a_acheter[$index]=0;//Initialisation du nombre de magasin de ce type qu'on achète.
 		$prix_du_magasin[$index]=pow(10,$index); // Initialisation des prix.
 		echo $magasins[$index]." : "; // On affiche le texte lié au magasin.			
-		echo "<input type=\"text\" name=\"["."\$magasin_a_acheter[\$index]"."]" . " \" />";
+		echo "<input type=\"text\" name=\"["."\$magasin_a_acheter[\$index]"."]"; //On nomme le formulaire pour acheter des magasins.
+		echo "\" class=\" formulaire\"";
+		echo " \" value=\"0\"/>"; //On assigne la valeur 0 à chacun des champs.
 		echo "<br>";
 	}
 	echo "<input type=submit value=\"Acheter des magasins.\">";
 }
 ?>
+
 </body>
 </html>
