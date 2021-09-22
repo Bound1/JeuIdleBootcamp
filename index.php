a7b000aa374bc239be06749b64f1d88597420645
Boubacar Pelage <boubacar.pelage@ynov.com>
2021-09-22 15:36:12 +0200

    Ajout des couleurs.

--- index.php
+++ index.php
@@ -21,10 +21,17 @@ session_start();
 function arrondir_dollars($value){
 	return floor($value*100)/100;
 }
+function nombre_en_couleur($nombre){
+	$couleur_RGB=array("red" => 0,"green" => 0,"blue" => 0);
+	$couleur_RGB["bleu"]=$nombre % 256;
+	$couleur_RGB["vert"]= floor($nombre / 256) % 256;
+	$couleur_RGB["rouge"]=((floor($nombre / 256) % 256) / 256) % 256;
+	return $couleur_RGB;
+}
 function achat($nombre_magasins_tableau,$prix_tableau,$argent_restant){
 	$argent=$argent_restant;
 	for($index=0;$index<count($nombre_magasins_tableau);$index++){
-		$argent = $argent-$nombre_magasins_tableau[$index]*$prix_tableau[$index];
+		$argent = $argent - $nombre_magasins_tableau[$index]*$prix_tableau[$index];
 		$_SESSION["magasins_possedes"][$index]+=$nombre_magasins_tableau[$index];
 		if($argent<0){
 			$argent=$argent_restant;	
@@ -38,8 +45,7 @@ function secondes_passes(){
 	$temps_initial=$_SESSION["temps_actualisation"];
 	$_SESSION["temps_actualisation"]=getdate()[0];		
 	$temps_fin=getdate()[0];
-	$valeur_retourne=$temps_fin;
-	$valeur_retourne-=$temps_initial;
+	$valeur_retourne=$temps_fin - $temps_initial;
 	return $valeur_retourne;
 }
 function actualiser_page($initialisation=false){
@@ -55,10 +61,10 @@ function actualiser_page($initialisation=false){
 		$nombre_colonnes_formulaire_jeu=$_SESSION["nombre_colonnes_formulaire_jeu"];
 	}
 	else{
-		$nombre_total_de_magasins=5;
-		$magasins=array("Stand de limonade","Médias", "Nettoyage de voiture", "Pizza", "Magasin de donut");	
+		$nombre_total_de_magasins=6;
+		$magasins=array("Stand de limonade","Médias", "Nettoyage de voiture", "Pizza", "Magasin de donut", "Bateaux de pêche");	
 		$gains_par_seconde=array();
-		$argent=1.0;
+		$argent=0.0;
 		$temps_actualisation=getdate()[0];
 		$magasins_possedes=array();
 		$magasin_a_acheter=array();
@@ -68,8 +74,10 @@ function actualiser_page($initialisation=false){
 		for($index=0;$index<$nombre_total_de_magasins;$index++){
 			$magasins_possedes[$index]=0; //Initialisation de la valeur des magasins.
 			$magasin_a_acheter[$index]=0;//Initialisation du nombre de magasin de ce type qu'on achète.
-			$prix_du_magasin[$index]=pow(10,$index); // Initialisation des prix.
+			$prix_du_magasin[$index]=pow(5,$index); // Initialisation des prix.
+			$prix_du_magasin[$index]=arrondir_dollars($prix_du_magasin[$index]);
 			$gains_par_seconde[$index]=$prix_du_magasin[$index]/2;
+			$gains_par_seconde[$index]=arrondir_dollars($gains_par_seconde[$index]);
 		}		
 		$_SESSION["deja_initialise"]=true;
 		$_SESSION["argent"]=$argent;
@@ -89,20 +97,23 @@ function actualiser_page($initialisation=false){
 	echo		"<input type=\"submit\" style=\"font-size:x-large\" name=\"cliquer_pour_gain\"  value=\" Cliquer (+".$gain_par_clic.")";
 	echo 			"/Mettre à jour\">";	
 	echo 	"</form>";
-	echo "</div>";
 	echo "<div class=\"container\">";//On met le formulaire dans un container grâce à Bootstrap.
 	echo 		"<form action=\"index.php\" method=\"post\" >"; //On commence à mettre notre formulaire.
 	echo "<br>";
-	echo "</span>";
 	for($index=0;$index<$nombre_total_de_magasins;$index++){
+		$nombre_correspondant_a_la_couleur=floor( ((256  * 256 * 256 - 1) / 1.2 / $nombre_total_de_magasins) * $index);
+		$nombre_converti_en_couleur=nombre_en_couleur($nombre_correspondant_a_la_couleur);
+		$transparence=0.4;
 		$au_debut_de_la_ligne=( ($index % $nombre_colonnes_formulaire_jeu) == 0);
 		if($au_debut_de_la_ligne){
 			echo "<div class=\"row\">"; 
 		}
-		echo "<div class=\"col\">";
+		echo "<div class=\"col\"";
+		echo " style=\"background-color: rgba(" . $nombre_converti_en_couleur["rouge"] . ", " . $nombre_converti_en_couleur["vert"] . ", " . $nombre_converti_en_couleur["bleu"] . ", " . $transparence . ");\" >";
 		echo $magasins[$index]." : ". "<br>"; // On affiche le texte lié au magasin.			
-		echo "<input type=\"number\" name=\""."magasin_a_acheter[$index] class= \"form-control"; //On nomme le formulaire pour acheter des magasins.
-		echo "\" value=\"0\"/>"; //On assigne la valeur 0 à chacun des champs.
+		echo "<input type=\"number\" name="."\"magasin_a_acheter[$index]\" class= \"form-control"; //On nomme le formulaire pour acheter des magasins.
+			
+		echo "\" value=\"0\">"; //On assigne la valeur 0 à chacun des champs.
 		echo "<br>";
 		echo "<span class=\"etiquette\"> Prix : </span>".$prix_du_magasin[$index]; // On affiche les prix du magasin.
 		echo "<br>";
@@ -116,6 +127,8 @@ function actualiser_page($initialisation=false){
 		}
 		echo "</div>";
 	}	
+	echo "</div>";
+	echo "<form action=\"index.php\" method=\"post\" >";
 	echo "<input type=submit value=\"Acheter des magasins.\">"; //On affiche le bouton "Acheter des magasins".
 	echo "<br><br>";
 	echo "</form>";
@@ -134,6 +147,9 @@ if(!isset($_POST["restart"]) && !isset($_POST["magasin_a_acheter"])){ // Si l'in
 	}
 }
 if(isset($_POST["restart"])){
+	session_unset();
+	session_destroy();
+	session_start();
 	actualiser_page($initialisation=true);
 }
 if(isset($_POST["magasin_a_acheter"])){
